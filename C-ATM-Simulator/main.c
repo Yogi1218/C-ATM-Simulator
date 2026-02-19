b153280eff2828753a2841544592d91df27688e0
Yogi Patel <yogibaps2@gmail.com>
2026-02-19 14:48:59 +0530

    Initial Commit

---
+++ C-ATM-Simulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-ATM-Simulator
+//
+//  Created by Yogi Patel on 19/02/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
